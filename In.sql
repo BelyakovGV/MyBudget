cb0204dfa469878effcecd5f6bd86cc4e1a05948
BelyakovGV <BelyakovGV@gmail.com>
2026-03-14 22:54:57 +0500

    first commit

---
+++ In.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[In]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity(1,1), 
+    [Sum] MONEY NOT NULL, 
+    [Src] NVARCHAR(150) NOT NULL, 
+    [Dt] DATE NOT NULL default sysdatetime()
+)
